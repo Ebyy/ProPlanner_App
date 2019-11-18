21cae59c467d62ec244b94b7bf1f2f725aaa9712
Eberechi Ogunedo <eberechi.ogunedo@ceridian.com>
2019-11-18 12:38:56 -0500

    router and browser history set up

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "./HomePage";
-
+import { Router, Route } from "react-router-dome";
 export const Main = () => {
   return (
-    <Provider store={store}>
+      <Router history ={}>
+              <Provider store={store}>
       <ConnectedDashboard />
     </Provider>
+      </Router>
+
   );
 };
